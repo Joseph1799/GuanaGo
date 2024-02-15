1623d1bacdcb382544d96ab8286eb03af2bb107d
Joseph1799 <Joseph1799us@gmail.com>
2024-02-14 22:37:28 -0600

    Website and Java API commit

---
+++ Rest-API-Java/appTurismo/src/main/java/com/guanago/appTurismo/AppTurismoApplication.java
@@ -0,0 +1,13 @@
+package com.guanago.appTurismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTurismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTurismoApplication.class, args);
+	}
+
+}
---
+++ Rest-API-Java/appTurismo/src/test/java/com/guanago/appTurismo/AppTurismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guanago.appTurismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTurismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
